d3134cd47b1e58768b75d827a74572aba121d181
Marlesios <marles.nuralyev@gmail.com>
2022-08-10 23:51:01 +0300

    Server updated

--- src/main/java/com/example/socket/Server.java
+++ src/main/java/com/example/socket/Server.java
@@ -6,37 +6,39 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class Server {
     public static void main(String[] args) throws IOException {
 
 
-        try{     ServerSocket serverSocket = new ServerSocket(8080);
-                 System.out.println("server started");
+          ServerSocket serverSocket = new ServerSocket(8080);
+                 out.println("server started");
 
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("client connected " + clientSocket.getRemoteSocketAddress());
+                out.println("client connected " + clientSocket.getRemoteSocketAddress());
                 Scanner scan = new Scanner(System.in);
 
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                while(true){
+              try(    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-                String msg;
-                String reply;
-                if((msg = in.readLine()) != null) {
-                    System.out.printf("hi %s, your port is %d \n", msg, clientSocket.getPort());
-                    System.out.println("Напишите ответ ");
-                    reply =scan.nextLine();
-                    out.println(reply);
-                }if("end".equals(msg)) {
-                    break;
-                }
-                    serverSocket.close();
-            }
-        }catch (IOException ex){
-            ex.printStackTrace();
+                  while (true) {
 
-        }
+                      String msg;
+                      String reply;
+                      if ((msg = in.readLine()) != null) {
+                          System.out.printf("Port # %d \nMessage: %s \n", clientSocket.getPort(), msg);
+                          System.out.println("Напишите ответ ");
+                          reply = scan.nextLine();
+                          out.println(reply);
+                      }
+                      if ("end".equals(msg)) {
+                          break;
+                      }
+                      serverSocket.close();
+                  }
+              }
+
     }
 }
